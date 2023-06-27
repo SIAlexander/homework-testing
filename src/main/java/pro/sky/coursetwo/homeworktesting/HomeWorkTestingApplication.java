680b55aa1c746297f61a88caf0522f23cf5f2deb
Smokvin <alex.smokvin@gmail.com>
2023-06-27 19:19:06 +0300

    initial commit

---
+++ src/main/java/pro/sky/coursetwo/homeworktesting/HomeWorkTestingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursetwo.homeworktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/coursetwo/homeworktesting/HomeWorkTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursetwo.homeworktesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
